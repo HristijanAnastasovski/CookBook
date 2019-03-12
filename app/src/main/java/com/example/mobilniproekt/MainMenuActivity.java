582d9ead1ba88fea1fdfe560ff4fb5b0a6bb7dd3
Hristijan <anastasovski.h@gmail.com>
2019-03-12 17:03:19 +0100

    Fully finished details activity. Added main menu and navigation

---
+++ app/src/main/java/com/example/mobilniproekt/MainMenuActivity.java
@@ -0,0 +1,64 @@
+package com.example.mobilniproekt;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+        Button btnToSearch = findViewById(R.id.mainMenuToSearchButton);
+        Button btnToFavorites = findViewById(R.id.mainMenuToFavoritesButton);
+        Button btnToAbout = findViewById(R.id.mainMenuToAboutButton);
+        Button btnToExit= findViewById(R.id.mainMenuToExitButton);
+
+        btnToSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentToSearch = new Intent(MainMenuActivity.this,MainActivity.class);
+                startActivity(intentToSearch);
+            }
+        });
+
+        btnToExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainMenuActivity.this);
+
+                builder.setTitle("Confirm");
+                builder.setMessage("Are you sure that you want to exit?");
+
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        finishAffinity();
+                        System.exit(0);
+
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // Do nothing
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/mobilniproekt/RecipeDetailsActivity.java
+++ app/src/main/java/com/example/mobilniproekt/RecipeDetailsActivity.java
@@ -3,10 +3,15 @@ package com.example.mobilniproekt;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -25,6 +30,9 @@ import com.example.mobilniproekt.retrofit.RetrofitClientInstance;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -37,6 +45,11 @@ public class RecipeDetailsActivity extends AppCompatActivity {
     private LinearLayoutManager linearLayoutManager;
     ProgressDialog progressDialog;
 
+    private ImageView imageView;
+    private TextView authorNameTextView ;
+    private Button fullGuideButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,19 +58,42 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String id = intent.getStringExtra(getString(R.string.id_details));
 
+        imageView = findViewById(R.id.detailsImage);
+        authorNameTextView =findViewById(R.id.authorNameTextView);
+        fullGuideButton = findViewById(R.id.buttonToFullRecipe);
+
         listInit();
         getDetailsData(id);
 
 
 
 
+
     }
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+
+    }*/
 
     public void getDetailsData(String id)
     {
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Loading....");
         progressDialog.show();
+
+       /* List<String> lista = new ArrayList<>();
+        String author="Author";
+        for(int i=1;i<=15;i++)
+        {
+            lista.add(Integer.toString(i));
+        }
+        lista.add("asdasasdadsasdadsadsadsdasasdasasdadsa sdadsads adsdas");
+        ingredientsAdapter.updateData(lista);
+        TextView authorNameTextView =(TextView) findViewById(R.id.authorNameTextView);
+        authorNameTextView.setText(author);
+        progressDialog.dismiss();*/
 
         /*Create handle for the RetrofitInstance interface*/
         GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
@@ -66,7 +102,7 @@ public class RecipeDetailsActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<RecipeDetailsWrapper> call, Response<RecipeDetailsWrapper> response) {
 
-                ImageView imageView = findViewById(R.id.detailsImage);
+
                 Glide.with(RecipeDetailsActivity.this)
                         .load(response.body().getRecipeDetails().getImage_url())
                         .transition(DrawableTransitionOptions.withCrossFade())
@@ -77,8 +113,19 @@ public class RecipeDetailsActivity extends AppCompatActivity {
 
                 setTitle(response.body().getRecipeDetails().getTitle());
                 ingredientsAdapter.updateData(response.body().getRecipeDetails().getIngredients());
-                TextView authorNameTextView =(TextView) findViewById(R.id.authorNameTextView);
                 authorNameTextView.setText(response.body().getRecipeDetails().getPublisher());
+                fullGuideButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String url = response.body().getRecipeDetails().getSource_url();
+                        Intent fullGuideIntent = new Intent(Intent.ACTION_VIEW);
+                        fullGuideIntent.setData(Uri.parse(url));
+                        startActivity(fullGuideIntent);
+                    }
+                });
+
+
+
 
 
 
@@ -100,5 +147,6 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(ingredientsAdapter);
+
     }
 }
